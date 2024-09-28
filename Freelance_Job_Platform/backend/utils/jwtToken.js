0778461e55e5efd3949f77b87fed14f6ceffa21e
Hein Htoo Naing <u6440027@au.edu>
2024-09-28 15:48:25 +0700

    Update jwtToken.js

--- Freelance_Job_Platform/backend/utils/jwtToken.js
+++ Freelance_Job_Platform/backend/utils/jwtToken.js
@@ -16,20 +16,3 @@ export const sendToken = (user, statusCode, res, message) => {
     });
   };
   
-=======
-  const token = user.getJWTToken();
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true, // Set httpOnly to true
-  };
-
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    user,
-    message,
-    token,
-  });
-};
->>>>>>> 6ebb4cb1eda6464260e326b98b123f73a0eb1869
